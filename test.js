bf4b142622bf077f6b29a93eefd141e227b9493a
pratik <lazy.monk.13@gmail.com>
2018-08-25 16:13:11 -0700

    First Commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('test 1', function(done){  
+      assert.equal(-1, [1,2,3].indexOf(4))
+      done()
+    })
+  })
+})
